18b44d12dc3c38d98dbc67dadea6eab8a9a5bb5d
Damian Dziaduch <damian@dziaduch.pl>
2019-08-08 07:23:53 +0200

    Start work on Pascal's Triangle - PHP

---
+++ php/pascalsTriangle.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+// https://www.codewars.com/kata/5226eb40316b56c8d500030f/train/php
+
+function pascals_triangle($n) {
+    // Your code here
+}
+
+assert([1] === pascals_triangle(1));
+assert([1, 1, 1] === pascals_triangle(2));
+assert([1, 1, 1, 1, 2, 1] === pascals_triangle(3));
+assert([1, 1, 1, 1, 2, 1, 1, 3, 3, 1] === pascals_triangle(4));
+assert([1, 1, 1, 1, 2, 1, 1, 3, 3, 1, 1, 4, 6, 4, 1] === pascals_triangle(5));
