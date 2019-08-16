7b0c000d7752a7401039cfb4ccc8362ad344bf58
Damian Dziaduch <damian@dziaduch.pl>
2019-08-16 09:30:59 +0200

    Add missing URLs

--- sql/SQL Basics: Simple JOIN and RANK.sql
+++ sql/SQL Basics: Simple JOIN and RANK.sql
@@ -1,3 +1,4 @@
+-- https://www.codewars.com/kata/58094559c47d323ebd000035
 SELECT people.*,
        count(sales.id) AS sale_count,
        rank() OVER (ORDER BY count(sales.id) DESC) AS sale_rank
--- sql/SQL Basics: Simple table totaling.sql
+++ sql/SQL Basics: Simple table totaling.sql
@@ -1,3 +1,4 @@
+-- https://www.codewars.com/kata/5809575e166583acfa000083
 SELECT RANK() OVER (ORDER BY sum(points) DESC),
        CASE WHEN clan = '' THEN '[no clan specified]' ELSE clan END,
        sum(points) AS total_points,
