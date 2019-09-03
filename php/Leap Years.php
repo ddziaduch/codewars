f6d5423b492d6b65835bdaeb05babbfcf6cbee9e
Damian Dziaduch <damian@dziaduch.pl>
2019-09-03 07:39:41 +0200

    Amend PHP Leap Year

--- php/Leap Years.php
+++ php/Leap Years.php
@@ -5,13 +5,7 @@ declare(strict_types=1);
 // https://www.codewars.com/kata/leap-years/train/php
 
 function isLeapYear(int $year): bool {
-    if ($year % 4 === 0) {
-        if ($year % 100 === 0) {
-            return $year % 400 === 0;
-        }
-        return true;
-    }
-    return false;
+    return $year % 400 === 0 || $year % 4 === 0 && $year % 100 !== 0;
 }
 
 assert(true === isLeapYear(2016));
