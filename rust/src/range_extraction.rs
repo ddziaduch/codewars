809c690177f25f7e2e864d3d3e769f16699f63e4
Damian Dziaduch <damian@dziaduch.pl>
2019-10-11 15:21:31 +0200

    Rust - Range Extraction - WIP

--- rust/src/range_extraction.rs
+++ rust/src/range_extraction.rs
@@ -9,28 +9,31 @@ fn range_extraction(numbers: &[i32]) -> String {
     let mut current_range: Vec<&i32> = Vec::new();
 
     for (index, number) in vector_of_numbers.iter().enumerate() {
-        if index == 0 {
-            current_range.push(number);
+        current_range.push(number);
+        let next_number = vector_of_numbers.get(index + 1);
+        if next_number.is_none() || next_number.is_some() && next_number.unwrap() - *number != 1  {
             ranges.push(current_range);
             current_range = Vec::new();
-        } else {
-            let prev_number = vector_of_numbers.get(index - 1);
-            if prev_number.is_some() && prev_number.unwrap() + 1 == *number && ranges.last().is_some() {
-                current_range.push(number);
-            } else {
-                current_range.push(number);
-                ranges.push(current_range);
-                current_range = Vec::new();
-            }
         }
     }
 
     println!("{:?}", ranges);
 
-    String::new()
+    let string = ranges.iter().map(|vector| {
+        match vector.len() {
+            0 => String::new(),
+            1 => format!("{}", vector.first().unwrap()),
+            _ => format!("{}-{}", vector.first().unwrap(), vector.last().unwrap())
+        }
+    }).collect::<Vec<String>>().join(",");
+
+    println!("{:?}", string);
+
+    string
 }
 
 pub fn verify() {
-    assert_eq!("-6,-3-1,3-5,7-11,14,15,17-20", range_extraction(&[-6,-3,-2,-1,0,1,3,4,5,7,8,9,10,11,14,15,17,18,19,20]));
-    assert_eq!("-3--1,2,10,15,16,18-20", range_extraction(&[-3,-2,-1,2,10,15,16,18,19,20]));
+    assert_eq!("-6,-3-1,3-5,7-11,14-15,17-20", range_extraction(&[-6,-3,-2,-1,0,1,3,4,5,7,8,9,10,
+        11,14,15,17,18,19,20]));
+    assert_eq!("-3--1,2,10,15-16,18-20", range_extraction(&[-3,-2,-1,2,10,15,16,18,19,20]));
 }
