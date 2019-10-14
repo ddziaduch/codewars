2cea48f4c6214cb63caced29e72b3d576f7554de
Damian Dziaduch <damian@dziaduch.pl>
2019-10-14 08:20:46 +0200

    Finish Rust Kata - range extraction

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,25 +1,25 @@
-//mod descending_order;
-//mod leap_years;
-//mod complementary_dna;
-//mod tribonacci;
-//mod wilson_prime;
-//mod create_phone_number;
-//mod playing_with_digits;
-//mod century_from_year;
-//mod two_to_one;
-//mod array_diff;
+mod descending_order;
+mod leap_years;
+mod complementary_dna;
+mod tribonacci;
+mod wilson_prime;
+mod create_phone_number;
+mod playing_with_digits;
+mod century_from_year;
+mod two_to_one;
+mod array_diff;
 mod range_extraction;
 
 fn main() {
-//    descending_order::verify();
-//    leap_years::verify();
-//    complementary_dna::verify();
-//    tribonacci::verify();
-//    wilson_prime::verify();
-//    create_phone_number::verify();
-//    playing_with_digits::verify();
-//    century_from_year::verify();
-//    two_to_one::verify();
-//    array_diff::verify();
+    descending_order::verify();
+    leap_years::verify();
+    complementary_dna::verify();
+    tribonacci::verify();
+    wilson_prime::verify();
+    create_phone_number::verify();
+    playing_with_digits::verify();
+    century_from_year::verify();
+    two_to_one::verify();
+    array_diff::verify();
     range_extraction::verify();
 }
--- rust/src/range_extraction.rs
+++ rust/src/range_extraction.rs
@@ -6,34 +6,29 @@ fn range_extraction(numbers: &[i32]) -> String {
     vector_of_numbers.sort();
 
     let mut ranges: Vec<Vec<&i32>> = Vec::new();
-    let mut current_range: Vec<&i32> = Vec::new();
+    let mut range: Vec<&i32> = Vec::new();
 
     for (index, number) in vector_of_numbers.iter().enumerate() {
-        current_range.push(number);
+        range.push(number);
         let next_number = vector_of_numbers.get(index + 1);
         if next_number.is_none() || next_number.is_some() && next_number.unwrap() - *number != 1  {
-            ranges.push(current_range);
-            current_range = Vec::new();
+            ranges.push(range);
+            range = Vec::new();
         }
     }
 
-    println!("{:?}", ranges);
-
-    let string = ranges.iter().map(|vector| {
-        match vector.len() {
+    ranges.iter().map(|range| {
+        match range.len() {
             0 => String::new(),
-            1 => format!("{}", vector.first().unwrap()),
-            _ => format!("{}-{}", vector.first().unwrap(), vector.last().unwrap())
+            1 => format!("{}", range.first().unwrap()),
+            2 => format!("{},{}", range.first().unwrap(), range.last().unwrap()),
+            _ => format!("{}-{}", range.first().unwrap(), range.last().unwrap())
         }
-    }).collect::<Vec<String>>().join(",");
-
-    println!("{:?}", string);
-
-    string
+    }).collect::<Vec<String>>().join(",")
 }
 
 pub fn verify() {
-    assert_eq!("-6,-3-1,3-5,7-11,14-15,17-20", range_extraction(&[-6,-3,-2,-1,0,1,3,4,5,7,8,9,10,
+    assert_eq!("-6,-3-1,3-5,7-11,14,15,17-20", range_extraction(&[-6,-3,-2,-1,0,1,3,4,5,7,8,9,10,
         11,14,15,17,18,19,20]));
-    assert_eq!("-3--1,2,10,15-16,18-20", range_extraction(&[-3,-2,-1,2,10,15,16,18,19,20]));
+    assert_eq!("-3--1,2,10,15,16,18-20", range_extraction(&[-3,-2,-1,2,10,15,16,18,19,20]));
 }
